2a2c8450ed104e68ae316a56a37dcc9e6f316b05
Milesgui23 <102737331+Milesgui23@users.noreply.github.com>
2022-05-05 09:00:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log(10 + 20 * 30);
+console.log(4**2 / 30);
+console.log((9**4+2)*6-1);
+console.log(10 % 3 * 10 ** 1 + 1 - 10 * 4 / 2)
+console.log('Miles')
+
+const x = 3;
+const y = 5
+
+console.log(2*x*3*y)
+
+const n1 = 3;
+const n2 = 5;
+const n3 = 7;
+
+console.log(n1+n2+n3);
+
+const salario = 1800
+
+console.log(salario*2)
